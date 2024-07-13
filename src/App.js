e1f99aeca11b0c9960177bfcc544384874a6e6c1
Nguyen Danh Nam <namnguyen1761998@gmail.com>
2024-07-13 18:00:23 +0700

    Change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I want to be free of mind
         </a>
       </header>
     </div>
